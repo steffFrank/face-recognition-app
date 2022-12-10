117fd40f0589ca530bbc2327144c444672f46c9a
steffFrank <--replace-all>
2022-12-10 11:13:43 +0100

    feat: Add a background image

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
-
+import "./App.css";
+import { Navigation } from "./Components/Navigation/Navigation";
+import { Image } from "./Components/Image/Image";
+import { Input } from "./Components/Input/Input";
 
 export const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Navigation />
+      <Input />
+      <Image />
+    </>
   )
 }
