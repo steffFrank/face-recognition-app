ba760c4fb397d2e1389093e263d506a0164ed1b6
steffFrank <--replace-all>
2022-12-10 13:58:51 +0100

    feat: Add the rank component

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./input.css";
-
+import { Rank } from "../Rank/Rank";
 
 export const Input = () => {
     return (
         <div className="form">
+            <Rank />
             <p>The magic brain will detect faces in your pictures.Give it a try</p>
             <div>
                 <input className="form__input"></input>
---
+++ src/Components/Rank/Rank.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./rank.css";
+
+
+export const Rank = () => {
+    return (
+        <div className="rank">
+            <h1>User your Rank is</h1>
+            <h2>#5</h2>
+        </div>
+    )
+}
