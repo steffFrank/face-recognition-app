9da92f6c3f0031c0841882d9592a30b7db4e2e19
steffFrank <--replace-all>
2022-12-15 20:41:47 +0100

    refactor: Send user data after signin

--- face-recognition-api/server.js
+++ face-recognition-api/server.js
@@ -33,7 +33,7 @@ app.get("/", (req, res) => {
 
 app.post("/signin", (req, res) => {
     if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-        res.status(200).json("success signing in");
+        res.status(200).json(database.users[0]);
     } else {
         res.status(400).json("error loggin in!");
     }
