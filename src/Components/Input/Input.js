b542394ac13fd73eda6d49a5ef7328c46b6f899f
steffFrank <--replace-all>
2022-12-17 08:08:43 +0100

    feat: Disable the detect button if no input is provided

--- src/Components/Input/Input.js
+++ src/Components/Input/Input.js
@@ -7,7 +7,7 @@ export const Input = ({onSubmit, input, onInputChange}) => {
             <p>The magic brain will detect faces in your pictures.Give it a try</p>
             <div>
                 <input type="text" value={input} onChange={onInputChange} className="form__input"></input>
-                <button type="submit" onClick={onSubmit} className="btn form__button">Detect</button>    
+                <button type="submit" onClick={onSubmit} disabled={input ? false : true} className="btn form__button">Detect</button>    
             </div>
         </form>
     )
