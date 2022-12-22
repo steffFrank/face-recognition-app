77c9f824238c2c7a289cf4edda0dd1d835294e1e
steffFrank <--replace-all>
2022-12-22 07:00:23 +0100

    style: format the code

--- src/Components/Register/Register.js
+++ src/Components/Register/Register.js
@@ -1,55 +1,78 @@
 import React, { useState } from "react";
 import "../SIGNIN/signin.css";
 
+export const Register = ({ onRouteChange, loadUser }) => {
+  const [register, setRegister] = useState({
+    name: "",
+    email: "",
+    password: "",
+  });
 
-export const Register = ({onRouteChange, loadUser}) => {
-    const [register, setRegister] = useState({name:"", email:"", password:""});
+  const onNameChange = (event) => {
+    setRegister((prevState) => {
+      return { ...prevState, name: event.target.value };
+    });
+  };
 
-    const onNameChange = (event) => {
-        setRegister(prevState => {
-            return {...prevState, name:event.target.value}
-        })
-    }
+  const onEmailChange = (event) => {
+    setRegister((prevState) => {
+      return { ...prevState, email: event.target.value };
+    });
+  };
+  const onPasswordChange = (event) => {
+    setRegister((prevState) => {
+      return { ...prevState, password: event.target.value };
+    });
+  };
 
-    const onEmailChange = (event) => {
-        setRegister(prevState => {
-            return {...prevState, email:event.target.value}
-        })
-    }
-    const onPasswordChange = (event) => {
-        setRegister(prevState => {
-            return {...prevState, password:event.target.value}
-        })
-    }
+  const onSubmitChange = (event) => {
+    event.preventDefault();
+    fetch("http://localhost:3001/register", {
+      method: "post",
+      headers: { "content-Type": "application/json" },
+      body: JSON.stringify(register),
+    })
+      .then((response) => response.json())
+      .then((user) => {
+        if (user.id) {
+          loadUser(user);
+          onRouteChange("signin");
+        }
+      });
+  };
 
-    const onSubmitChange = (event) => {
-        event.preventDefault();
-        fetch("http://localhost:3001/register", {
-            method: "post",
-            headers: {"content-Type": "application/json"},
-            body: JSON.stringify(register)
-        }).then(response => response.json())
-          .then(user => {
-            if (user.id) {
-                loadUser(user);
-                onRouteChange("signin");
-            }
-          });
-
-    }
-
-    return (
-        <main className="main" >
-            <form className="main__form">
-                <h2>Register</h2>
-                <label htmlFor="last_name">Name</label>
-                <input onChange={onNameChange} name="last_name" className="last_name" type="text" autoComplete="username"></input>
-                <label htmlFor="email">Email</label>
-                <input onChange={onEmailChange} name="email" className="email" type="email" autoComplete="username"></input>
-                <label htmlFor="password">Password</label>
-                <input onChange={onPasswordChange} name="password" className="password" autoComplete="new-password" type="password"></input>
-                <button  onClick={onSubmitChange} className="btn btn__signin">Register</button>
-            </form>
-        </main>
-    )
-}
+  return (
+    <main className="main">
+      <form className="main__form">
+        <h2>Register</h2>
+        <label htmlFor="last_name">Name</label>
+        <input
+          onChange={onNameChange}
+          name="last_name"
+          className="last_name"
+          type="text"
+          autoComplete="username"
+        ></input>
+        <label htmlFor="email">Email</label>
+        <input
+          onChange={onEmailChange}
+          name="email"
+          className="email"
+          type="email"
+          autoComplete="username"
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          onChange={onPasswordChange}
+          name="password"
+          className="password"
+          autoComplete="new-password"
+          type="password"
+        ></input>
+        <button onClick={onSubmitChange} className="btn btn__signin">
+          Register
+        </button>
+      </form>
+    </main>
+  );
+};
